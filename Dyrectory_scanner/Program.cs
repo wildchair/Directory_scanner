34d10ba04ed90fe197145756ae62734eeaf84bc3
tonkikh.danila <tonkikh.danila@yandex.ru>
2023-06-23 02:53:53 +0300

    Добавьте файлы проекта.

---
+++ Dyrectory_scanner/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+/*Недостатком данного решения является привязка к расширению файла, что может привести к некорректному
+ анализу MimeType файлов, чьи расширения были изменены. Альтернативным вариантом решения был бы побитовый
+ анализ файлов с составлением собственного словаря сопоставлений. Исходя из ТЗ считаю допустимым 
+ использование встроенных средств из System.Web.MimeMapping*/
+
+namespace Directory_scanner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MimeMapping.GetMimeMapping("C:\\ScheduleMADI\\LICENSE.txt"));
+        }
+    }
+}
